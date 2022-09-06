6e5167e2507ffde1225a655183c94e3e8ba047c6
sufiyanbmk <sufiyanbmk@gmail.com>
2022-09-06 10:32:11 +0530

    typo fixes and added btn color

--- js/validate.js
+++ js/validate.js
@@ -1,95 +1,96 @@
 
-function namevalidate(){
-    let name=document.getElementById("name").value
-    let error=document.querySelector(".name-error")
-    if(name.trim()==''){
-        error.innerHTML='Enter Full Name';
+function namevalidate() {
+    let name = document.getElementById("name").value;
+    let error = document.querySelector(".name-error");
+    if (name.trim() == '') {
+        error.innerHTML = 'Enter Full Name';
         return false;
     }
-    else if(name.length<4){
-        error.innerHTML='Name Must Be Atleast Four Character';
+    else if (name.length < 4) {
+        error.innerHTML = 'Name Must Be Atleast Four Character';
         return false;
     }
-    else if(!name.match(/^[a-zA-Z ]+$/)){
-        error.innerHTML='Enter valid name';
+    else if (!name.match(/^[a-zA-Z ]+$/)) {
+        error.innerHTML = 'Enter valid name';
         return false;
     }
-    else{
-        error.innerHTML=''
+    else {
+        error.innerHTML = '';
         return true;
     }
 }
-function emailvalidate(){
-    var email=document.getElementById("email").value;
-    var error1=document.getElementById("email-error")
-    if(email==''){
-        error1.innerHTML='Enter a Email';
+function emailvalidate() {
+    var email = document.getElementById("email").value;
+    var error1 = document.getElementById("email-error");
+    if (email == '') {
+        error1.innerHTML = 'Enter a Email';
         return false;
     }
-    
-    else{
-        error1.innerHTML='';
+
+    else {
+        error1.innerHTML = '';
         return true;
     }
 }
-        
-function phonevalidate(){
-    var phone=document.getElementById("phoneno").value
-    var error2=document.getElementById("number-error")
-    if(phone==''){
-        error2.innerHTML='Enter a Phone no';
+
+function phonevalidate() {
+    var phone = document.getElementById("phoneno").value;
+    var error2 = document.getElementById("number-error");
+    if (phone == '') {
+        error2.innerHTML = 'Enter a Phone no';
         return false;
     }
-    else if(phone.length<10){
-        error2.innerHTML='please Enter 10 Number';
+    else if (phone.length < 10) {
+        error2.innerHTML = 'please Enter 10 Number';
         return false;
     }
-    else if(!phone.match(/^\d{10}$/)){
-        error2.innerHTML='Enter valid number';
+    else if (!phone.match(/^\d{10}$/)) {
+        error2.innerHTML = 'Enter valid number';
         return false;
     }
-    else{
-        error2.innerHTML='';
+    else {
+        error2.innerHTML = '';
         return true;
     }
 }
-function messagevalidate(){
-    var message=document.getElementById("message").value
-    var error3=document.getElementById("message-error")
-    if(message==''){
-        error3.innerHTML='Enter a Message'
+function messagevalidate() {
+    var message = document.getElementById("message").value;
+    var error3 = document.getElementById("message-error");
+    if (message == '') {
+        error3.innerHTML = 'Enter a Message'
         return true
     }
-    else{
-        error3.innerHTML='';
+    else {
+        error3.innerHTML = '';
         return true;
     }
 
 }
 
-function validation(){
+function validation() {
     namevalidate();
     emailvalidate();
     phonevalidate();
     messagevalidate();
-    if(namevalidate()==false){
+    if (namevalidate() == false) {
         return false;
     }
-   
-    else if(emailvalidate()==false){
+
+    else if (emailvalidate() == false) {
         return false;
     }
-    else if(phonevalidate()==false){
+    else if (phonevalidate() == false) {
         return false;
     }
-    else if(messagevalidate()==false){
+    else if (messagevalidate() == false) {
         return false;
     }
-    else{
+    else {
+        document.getElementById("submit-btn").style.background = "green";
         return true;
-       
     }
 }
+
 
 // const scriptURL="https://script.google.com/macros/s/AKfycbzGZ5XK5fHauJSU18gn4_rbHLTaNiqdodMG1sm7IT4iDu5HrGnKOqErfXMA5_8DmY-0-g/exec"
 
